14390a98a8901ff533b4b2e2c4735f90487ae36c
icydwei <robpaik@gmail.com>
2019-09-09 16:31:21 -0400

    prior to pull for seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :username, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :interests, through: :user_interests
     has_many :user_destinations
     has_many :destinations, through: :user_destinations
+
+    validates_presence_of :name, :username, :password
+
 end
