b64f57283e9c0a3ee52a7e5ec33343e8d9f802bd
p6doyle <p6doyle@gmail.com>
2019-09-09 15:30:59 -0400

    pushing up views?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 ###Users
+peyton = User.find_or_create_by(name: "Peyton Doyle")
+rob = User.find_or_create_by(name: "Rob Paik")
+marisa = User.find_or_create_by(name: "Marisa Canan-Zucker")
+pete = User.find_or_create_by(name: "Pete Hanner")
+victor = User.find_or_create_by
 
 ###Destinations
+xian =
+
+
 
 ###Locales
 
