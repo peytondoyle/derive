00f6b5e495694322347b812ec96799cd7ddbdc7c
icydwei <robpaik@gmail.com>
2019-09-13 13:35:09 -0400

    custom routes created for add/remove derive destinations on usershow page - to differentiate from add/remove on destination show page

--- app/controllers/user_destinations_controller.rb
+++ app/controllers/user_destinations_controller.rb
@@ -6,10 +6,24 @@ class UserDestinationsController < ApplicationController
     redirect_to destination_path(@user_destination.destination)
   end
 
+  def remove
+    @user_destination = UserDestination.find(params[:id])
+    @user_destination.destroy
+    redirect_to user_path(@user_destination.user)
+  end
+
   def create
     @destination = Destination.find(params[:id])
     @user = User.find_by(username: session[:user])
     @user_destination = UserDestination.find_or_create_by(user: @user, destination: @destination)
     redirect_to destination_path(@destination)
   end
+
+  def add
+    @destination = Destination.find(params[:id])
+    @user = User.find_by(username: session[:user])
+    @user_destination = UserDestination.find_or_create_by(user: @user, destination: @destination)
+    redirect_to user_path(@user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post '/login', to: 'users#verify', as: 'verify'
   delete '/logout', to: 'users#logout', as: 'logout'
   post '/user_destinations/:id', to: 'user_destinations#create', as: 'user_destinations_create'
+  post '/user_destinations/:id/add', to: 'user_destinations#add', as: 'user_destination_add'
+  delete '/user_destinations/:id/remove', to: 'user_destinations#remove', as: 'user_destination_remove'
   resources :destination_locales
   resources :locales
   resources :destination_interests
